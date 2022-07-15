35ae9f0d8405877042097a03cba034bd237871c9
Obolikhin.Fedor <Fedor.Obolikhin@amonitoring.ru>
2022-07-15 17:43:50 +0300

    add middleware.go

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"go_test/internal/apperror"
 	"go_test/pkg/logging"
 	"go_test/pkg/utils"
 	"net/http"
@@ -26,53 +27,65 @@ func NewHandler(logger *logging.Logger) handlers.Handler {
 }
 
 func (h *handler) Register(router *httprouter.Router) {
-	router.GET(usersURL, h.GetList)
-	router.GET(userURL, h.GetUserByUUID)
-	router.POST(usersURL, h.CreateUser)
-	router.PUT(userURL, h.UpdateUser)
-	router.PATCH(userURL, h.PartiallyUpdateUser)
-	router.DELETE(userURL, h.DeleteUser)
+	router.HandlerFunc(http.MethodGet, usersURL, apperror.Middleware(h.GetList))
+	router.HandlerFunc(http.MethodGet, userURL, apperror.Middleware(h.GetUserByUUID))
+	router.HandlerFunc(http.MethodPost, usersURL, apperror.Middleware(h.CreateUser))
+	router.HandlerFunc(http.MethodPut, userURL, apperror.Middleware(h.UpdateUser))
+	router.HandlerFunc(http.MethodPatch, userURL, apperror.Middleware(h.PartiallyUpdateUser))
+	router.HandlerFunc(http.MethodDelete, userURL, apperror.Middleware(h.DeleteUser))
 
 }
 
-func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (h *handler) GetList(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(http.StatusOK)
 	_, err := w.Write([]byte("this is list of users"))
 	utils.PanicHandler(err)
 	h.logger.Info("get list users")
+
+	return nil
 }
 
-func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(http.StatusOK)
 	_, err := w.Write([]byte("this is get user"))
 	utils.PanicHandler(err)
 	h.logger.Info("get user by uuid")
+
+	return nil
 }
 
-func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(http.StatusCreated)
 	_, err := w.Write([]byte("this is create user"))
 	utils.PanicHandler(err)
 	h.logger.Info("create user")
+
+	return nil
 }
 
-func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(http.StatusOK)
 	_, err := w.Write([]byte("this is update user"))
 	utils.PanicHandler(err)
 	h.logger.Info("update user")
+
+	return nil
 }
 
-func (h *handler) PartiallyUpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (h *handler) PartiallyUpdateUser(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(http.StatusOK)
 	_, err := w.Write([]byte("this is pathupdate user"))
 	utils.PanicHandler(err)
 	h.logger.Info("partially update user")
+
+	return nil
 }
 
-func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(http.StatusNoContent)
 	_, err := w.Write([]byte("this is delete user"))
 	utils.PanicHandler(err)
 	h.logger.Info("delete user")
+
+	return nil
 }
